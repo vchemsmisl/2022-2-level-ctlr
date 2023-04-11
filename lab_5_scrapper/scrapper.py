cf56054470d35cfd1a1be6a4f55a52d16c0577ce
vchemsmisl <issakov.daanila@mail.ru>
2023-04-11 16:13:37 +0300

    types fixed [mark 8]

--- lab_5_scrapper/scrapper.py
+++ lab_5_scrapper/scrapper.py
@@ -191,7 +191,7 @@ class Crawler:
         url: Union[str, list, None] = article_bs.get('href')
         if isinstance(url, str):
             return url
-        return None
+        return ''
 
     def find_articles(self) -> None:
         """
